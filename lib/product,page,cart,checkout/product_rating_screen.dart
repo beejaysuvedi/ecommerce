bdda711fd58da86bd0f269f6a5421df694ad36e5
beejaysuvedi <beejaysuvedi@gmail.com>
2026-08-03 09:33:15 +0545

    feat: enhance ProductRatingScreen with size selection and improved layout

--- lib/product,page,cart,checkout/product_rating_screen.dart
+++ lib/product,page,cart,checkout/product_rating_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/home/product_data_list.dart';
+import 'package:ecommerce/product,page,cart,checkout/cart_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -28,7 +29,11 @@ class ProductRatingScreen extends StatelessWidget {
                   CircleAvatar(
                     radius: 20,
                     backgroundColor: Colors.grey.withValues(alpha: 0.15),
-                    child: Icon(Icons.arrow_back, color: Colors.black,),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(Icons.arrow_back, color: Colors.black,)),
                   ),
                    CircleAvatar(
                     radius: 20,
@@ -94,7 +99,7 @@ class ProductRatingScreen extends StatelessWidget {
                      Row(
                       children: [
                            Text("s"),
-                           SizedBox(width: 5,),
+                           SizedBox(width:8,),
                         Icon(Icons.keyboard_arrow_down),
                       ],
                      )
@@ -114,6 +119,23 @@ class ProductRatingScreen extends StatelessWidget {
                     
           
                   ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("size"),
+                     Row(
+                      children: [
+                           Icon(Icons.circle),
+                           SizedBox(width: 8,),
+                        Icon(Icons.keyboard_arrow_down),
+                      ],
+                     ),
+                      ],
+                    ),
+                     ),
+                  
                   
                 ),
                  SizedBox(height: 10,),
@@ -125,6 +147,27 @@ class ProductRatingScreen extends StatelessWidget {
                     
           
                   ),
+                   child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("size"),
+                     Row(
+                      children: [
+                        Icon(Icons.add_circle, color: Color(0xff8E6CEF),),
+                        SizedBox(width: 8,),
+                        Text("1"),
+                          
+                           SizedBox(width: 8,),
+
+                          
+                        Icon(Icons.remove_circle,color: Color(0xff8E6CEF), ),
+                      ],
+                     ),
+                      ],
+                    ),
+                     ),
                   
                   
                 ),
@@ -140,15 +183,82 @@ class ProductRatingScreen extends StatelessWidget {
                 Text("213 Reviews", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),),
                 SizedBox(height: 10,),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  
                   children: [
-                    CircleAvatar(
+                    Row(
+                      children: [
+                        CircleAvatar(
                       radius: 20,
-                      backgroundImage: AssetImage(assetName),
+                      backgroundImage: AssetImage("assets/kalomanxe.png",),
 
+
+
+                    ),
+                    SizedBox(width: 7,),
+                    Text("Kaluwa"),
+                   
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        
+                        Icon(Icons.star, color:Color(0xff8E6CEF),),
+                        SizedBox(width: 5,),
+                        Icon(Icons.star, color:Color(0xff8E6CEF),),
+                        SizedBox(width: 5,),
+                        Icon(Icons.star, color:Color(0xff8E6CEF),),
+                        SizedBox(width: 5,),
+                       
+                        
+                      ],
 
                     )
                   ],
-                )
+                ),
+                Text("Gucci transcribes its heritage, creativity, and innovation into a plenitude of collections. From staple items to distinctive accessories.", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),),
+                SizedBox(height: 5,),
+                Text("12 days ago", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),),
+
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage("assets/goromanxe.png",),
+
+
+
+                    ),
+                    SizedBox(width: 7,),
+                    Text("Alex Morgan"),
+                   
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        
+                        Icon(Icons.star, color:Color(0xff8E6CEF),),
+                        SizedBox(width: 5,),
+                        Icon(Icons.star, color:Color(0xff8E6CEF),),
+                        SizedBox(width: 5,),
+                        Icon(Icons.star, color:Color(0xff8E6CEF),),
+                        SizedBox(width: 5,),
+                       
+                        
+                      ],
+
+                    )
+                  ],
+                ),
+                Text("Gucci transcribes its heritage, creativity, and innovation into a plenitude of collections. From staple items to distinctive accessories.", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),),
+                SizedBox(height: 5,),
+                Text("12 days ago", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),),
+                SizedBox(height: 50,),
 
 
                 
@@ -165,14 +275,30 @@ class ProductRatingScreen extends StatelessWidget {
               color: Colors.white
             ),
             child: Center(
-              child: Container(
-                width: 360,
-                height: 20,
-                decoration: BoxDecoration(
-                  color: Colors.indigo
-                ),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_)=>CartScreen()));
+                },
+                child: Container(
+                  width: 342,
+                  height: 52,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.indigo
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("\$148", style: TextStyle(color: Colors.white),),
+                        Text("Add to Bag", style: TextStyle(color: Colors.white),)
+                      ],
+                    ),
+                  ),
+                  
                 
-              
+                ),
               ),
             ),
           ))
