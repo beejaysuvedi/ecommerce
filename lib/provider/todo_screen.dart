a28c2835477b9a7986285303bf44a7f164ad5329
Beejay suvedi <143896626+beejaysuvedi@users.noreply.github.com>
2026-09-16 09:20:05 +0545

    feat: refactor TodoScreen to StatefulWidget and enhance todo management features

--- lib/provider/todo_screen.dart
+++ lib/provider/todo_screen.dart
@@ -1,16 +1,31 @@
+import 'package:ecommerce/provider/theme_provider.dart';
 import 'package:ecommerce/provider/todo_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class TodoScreen extends StatelessWidget {
+class TodoScreen extends StatefulWidget {
   const TodoScreen({super.key});
 
   @override
+  State<TodoScreen> createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  TextEditingController _Todocontroller =TextEditingController();
+  @override
   Widget build(BuildContext context) {
+    
     final todoProvider = Provider.of<TodoProvider>(context);
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo Screen"),
+        actions: [
+          Switch(value: themeProvider.isDarkTheme, onChanged: (_){
+            themeProvider.toogleTheme();
+          },)
+        ],
+        
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
@@ -20,13 +35,49 @@ class TodoScreen extends StatelessWidget {
               children: [
                 Expanded(
                   child: TextFormField(
+                    controller: _Todocontroller,
                   
                     
                   ),
+                  
                 ),
-                ElevatedButton(onPressed: (){}, child: Text("submit"))
+                ElevatedButton(onPressed: (){
+                  if(_Todocontroller.text.isEmpty){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: 
+                      Text("Please Input Field"), backgroundColor: Colors.red,),
+                      
+                    );
+                    
+
+                  }
+                  else{
+                    todoProvider.addTodo(_Todocontroller.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Todos Added"),
+                      backgroundColor: Colors.green,),
+                      
+                    );
+                  }
+                  _Todocontroller.clear();
+                }, child: Text("submit"))
               ],
-            )
+            ),
+            SizedBox(height: 20,),
+
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: todoProvider.todos.length,
+              itemBuilder:(context, index){
+                return ListTile(
+                  title: Text(todoProvider.todos[index].title),
+                  trailing: IconButton(onPressed: (){
+                    todoProvider.removeTodo(index);
+                  }, icon: Icon(Icons.delete,color: Colors.red,)),
+
+                );
+              }
+              ),
           ],
         ),
       ),
